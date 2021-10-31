cf33cc50c5440acbbcb21a3eaad9b7265ba8d46e
bodynight <xbodynightx@gmail.com>
2021-10-31 18:52:04 +0300

    update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 class Logger
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+#class method
   def self.say_something
     puts 'HaHa'    
   end
+
+#instance method
+  def log_samething what
+    @f.puts what
+  end
 end
 
+#вызов для class method
 Logger.say_something
+
+#вызов для instance method
+logger = Logger.new
+logger.log_samething 'hay !'
