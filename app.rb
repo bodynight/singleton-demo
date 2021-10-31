436737b2b8a800a4c58bfdff84038da2485590d6
bodynight <xbodynightx@gmail.com>
2021-10-31 20:51:21 +0300

    refactoring

--- app.rb
+++ app.rb
@@ -1,9 +1,4 @@
 require './logger'
 
-#вызов для class method
-Logger.instance.log_samething 'blabla!!!'
-
-# вызов для instance method
-# не будет работать после добавления private_class_method :new
-#     logger = Logger.new
-#     logger.log_samething 'hay !'
+Logger.instance.log_samething 'blabla6'
+# logger = Logger.new не будет работать
--- logger.rb
+++ logger.rb
@@ -1,22 +1,14 @@
+require 'singleton'
+
 class Logger
+  include Singleton
+
   def initialize
     @f = File.open 'log.txt', 'a'
   end
 
-  @@x = Logger.new
-  def self.instance # instance произвольное имя но принято так обозначать singleton
-    return @@x
-  end
-
-#class method
-  # def self.say_something
-  #   puts 'HaHa'    
-  # end
-
-#instance method
   def log_samething what
     @f.puts what
+    @f.flush # все изменения будут сброшены надиск
   end
-
-  private_class_method :new
 end
