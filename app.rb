cb2fee5f7477e227fe72a211dec053a4269b3c5e
bodynight <xbodynightx@gmail.com>
2021-10-31 19:13:28 +0300

    update logger to include self.instance

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ class Logger
     @f = File.open 'log.txt', 'a'
   end
 
+  @@x = nil
+  def self.instance
+    if @@x == nil
+      @@x = Logger.new
+    end
+    return @@x
+  end
+
 #class method
   def self.say_something
     puts 'HaHa'    
@@ -16,6 +24,8 @@ end
 
 #вызов для class method
 Logger.say_something
+Logger.instance.log_samething 'blabla'
+Logger.instance.log_samething 'blabla2'
 
 #вызов для instance method
 logger = Logger.new
